b34b1605226383463dc5e6696cbd6f02368e9504
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-14 12:14:59 +0900

    feat: 개발 DB를 H2에서 MySQL로 변경하고 트랜잭션 쿼리 로그 분석 추가

--- src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -5,6 +5,7 @@ import com.back.domain.wiseSaying.wiseSaying.service.WiseSayingService;
 import com.back.standard.util.service.MarkdownService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,6 +54,7 @@ public class WiseSayingController {
 
     @GetMapping("/wiseSayings/{id}")
     @ResponseBody
+    @Transactional
     public String detail(@PathVariable int id) {
         WiseSaying wiseSaying = wiseSayingService.findById(id).get();
 
